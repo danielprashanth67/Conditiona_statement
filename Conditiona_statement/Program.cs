a2e4b9512a06183d9c8a698796394f213258bdbe
daniel <naguri@mail.usf.edu>
2019-09-10 19:43:16 -0400

    Add project files.

---
+++ Conditiona_statement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Conditiona_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Experience");
+                string years = Console.ReadLine();
+                int years_of_exp = Int32.Parse(years);
+                switch (years_of_exp)
+                {
+                    case 0:
+                        Console.WriteLine("Need lot of practice");
+                        break;
+                    case 1:
+                        Console.WriteLine("Looks like you have little experience");
+                        break;
+                    case 2:
+                        Console.WriteLine("Wow You've been doing this for a little while");
+                        break;
+                    default:
+                        Console.WriteLine("You are an expert");
+                        break;
+                }
+            }
+            catch(InvalidCastException )
+            {
+                Console.WriteLine("Try giving the years of experience in string");
+            }
+
+        }
+    }
+}
